2d1110f06277d61acc9218fa8dbe252e45e60a02
Glasspham <hathimongthu1980@gmail.com>
2025-05-18 11:29:34 +0700

    update

---
+++ BackEnd/src/test/java/com/drugprevention/api/DrugPreventionApplication.java
@@ -0,0 +1,13 @@
+package com.drugprevention.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DrugPreventionApplication {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
