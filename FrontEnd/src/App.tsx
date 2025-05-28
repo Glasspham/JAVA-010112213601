ed173a3ed838352f86ad0662aa5f1b903bdd0d71
harrytien107 <zeentien.123@gmail.com>
2025-05-28 08:35:00 +0700

    Cấu trúc lại folder cần thiết cho project

---
+++ FrontEnd/src/App.tsx
@@ -0,0 +1,13 @@
+import './App.css';
+
+function App() {
+  // Print a message hello world
+  return (
+    <div>
+      <h1>Xin chào!</h1>
+      <p>Đây là ứng dụng React của bạn.</p>
+    </div>
+  );
+}
+
+export default App;
